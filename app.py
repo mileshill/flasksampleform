853110726dcccbde2f47ac0e83c802fb7dfdbe29
miles <miles1618@gmail.com>
2017-12-31 19:48:29 -0600

    Updated comments

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 from flask import Flask, render_template, request
 from forms.search_form import SearchForm
-#from flask_bootstrap import Bootstrap
 import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MyComplexPassword!234'
-#bootstrap = Bootstrap(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # Load the custom form;
+    # Forms are python classes that inherit from FlaskForm
     form = SearchForm()
-
   
+    # When the form is POST'ed from the site, the POST is back
+    # to this route. If all inputs are valid, the logic block is entered
     if form.validate_on_submit():
-        base_url = 'https://api.coinmarketcap.com/v1/ticker/'
+        # Call coinmarketcap.com for number of coins
+        # submitted in the form
         base_url = 'https://api.coinmarketcap.com/v1/ticker/?limit='
         search_coin = form.stringfield.data
         api_url = base_url + search_coin
 
+        # Results is a `list` of `dict`. [{k:v..}, ..]
         results = requests.get(api_url).json()
 
+        # Return the same page, blank form and all results
         return render_template('index.html', results=results, form=form)
 
     return render_template('index.html', form=form, type=request.method)
--- forms/search_form.py
+++ forms/search_form.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import InputRequired
+
 class SearchForm(FlaskForm):
+    """SearchForm supports a single string field."""
     stringfield = StringField('StringField', description="Top `N` coins", validators=[InputRequired()])
